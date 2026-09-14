20736f5e0d288b01b76f9e07f2d0a1b4bad093d3
Aditya <aditya@gmail.com>
2026-09-14 23:32:11 +0530

    beryl: Drop unsupported FM Radio, remove dead power-hal-1-2, and update VINTF

--- device.mk
+++ device.mk
@@ -168,10 +168,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# FM Radio
-PRODUCT_PACKAGES += \
-    RevampedFMRadio \
-    libmtkfmjni
 
 # Health
 PRODUCT_PACKAGES += \
@@ -238,7 +234,6 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,power_libperfmgr,mode_extension_lib, //$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor \
     libmtkperf_client \
     libpowerhalwrap_vendor
