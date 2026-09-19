84e664b542dcff983249226534714a449a96b3fd
Aditya <aditya@gmail.com>
2026-09-19 14:17:36 +0530

    Revert "beryl: Exclude Qualcomm HALs from Blueprint scan"
    This reverts commit 417c388f4a22232b590d3177dbda356556d50298.

--- device.mk
+++ device.mk
@@ -6,11 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/beryl
 
-# Exclude repos from bp scanning
+# Exclude repos from bp scanning to prevent duplicate module definitions with hardware/lineage/compat
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
 PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
-PRODUCT_SOURCE_ROOT_DIRS += -hardware/qcom
-PRODUCT_SOURCE_ROOT_DIRS += -hardware/qcom-caf
 
 PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
 
