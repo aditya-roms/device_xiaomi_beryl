a992b13b70783d90be422530c1c42a902bb2d822
Aditya <aditya@gmail.com>
2026-09-19 14:12:52 +0530

    beryl: Exclude Qualcomm HALs from Blueprint scan

--- device.mk
+++ device.mk
@@ -6,9 +6,11 @@
 
 DEVICE_PATH := device/xiaomi/beryl
 
-# Exclude repos from bp scanning to prevent duplicate module definitions with hardware/lineage/compat
+# Exclude repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
 PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
+PRODUCT_SOURCE_ROOT_DIRS += -hardware/qcom
+PRODUCT_SOURCE_ROOT_DIRS += -hardware/qcom-caf
 
 PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
 
